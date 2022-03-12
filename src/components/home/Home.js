44f497b43ea056696d50f43fb383493cb70f6738
Matteo Mileva <52746738+mattemile@users.noreply.github.com>
2022-03-12 17:06:36 +0100

    Add files via upload

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './css/home.css'
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Home
